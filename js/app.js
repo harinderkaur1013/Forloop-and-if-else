2140db0c922d190e64b20655b34ed8d32270ccb2
harinderkaur <kaurh1013@gmil.com>
2017-03-29 11:01:58 -0400

    forloop

---
+++ js/app.js
@@ -0,0 +1,203 @@
+var num = 0;
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+
+}
+
+//2
+var num = 0;
+for (var i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+//3
+var num = 0;
+for (var i = 0; i <= 50; i += 3) {
+    console.log(i);
+
+}
+
+//4
+var num = 0;
+for (var i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
+//5
+
+var num = 0;
+for (var i = 40; i <= 60; i++) {
+    console.log(i);
+}
+
+//6
+var num = 0;
+for (var i = 80; i <= 100; i++) {
+    console.log(i);
+}
+
+//7
+// var i = prompt("enter the number")
+// if (i = 1)
+//     for (i = 1; i <= 11; i++) {
+//         console.log(i);
+//     }
+
+// // else if (i == 2)
+// //     for (i; i <= 12; i++) {
+// //         console.log(i);
+// //     }
+// else if (i == 3)
+//     for (i; i <= 13; i++) {
+//         console.log(i);
+
+// }	    
+// else if (i == 4)
+//     for (i; i <= 14; i++) {
+//         console.log(i);
+//     }
+// else(i == 5)
+// for (i; i <= 15; i++) {
+//     console.log(i);
+// }
+//8
+var i = prompt("enter the number")
+if (i = 1)
+    for (i; i <= 123; i++) {
+        console.log("One,Two,Three")
+    }
+    // else if (i == 2)
+    //     for (i; i <= 125; i++) {
+    //         console.log("One,Two,Four")
+    //     }
+    // else if (i == 3)
+    //     for (i; i <= 126; i++) {
+    //         console.log("One,Two,Five")
+    //     }
+    // else if (i == 4)
+    //     for (i; i <= 127; i++) {
+    //         console.log("One,Two,Six")
+    //     }
+    // else(i == 5)
+    // for (i; i <= 128; i++) {
+    //     console.log("One,Two,Seven")
+    // }
+    //9
+var i = prompt("enter the number")
+if (i = 1)
+    for (i; i <= 1; i++) {
+        console.log("One")
+    }
+    // else if (i = 2)
+    //     for (i; i <= 2; i++) {
+    //         console.log("Two")
+    //     }
+    // else if (i = 3)
+    //     for (i; i <= 3; i++) {
+    //         console.log("Three")
+    //     }
+
+// else if (i = 4)
+//     for (i; i <= 4; i++) {
+//         console.log("four")
+//     }
+// else if (i = 5)
+//     for (i; i <= 5; i++) {
+
+//     }
+// else {
+//  console.log("valid number")
+
+// }
+//10
+var i = prompt("enter a number between 1 to 10")
+if (i = 8)
+    for (i; i <= 10; i++) {
+        console.log("1 and 10");
+    }
+    // else if (i = 11)
+    //     for (i; i <= 20; i++) {
+    //         console.log("11 and 20");
+    //     }
+    // else if (i = 21)
+    //     for (i; i <= 30; i++) {
+    //         console.log("21 and 30");
+    //     }
+    // else if (i = 31)
+    //     for (i; i <= 40; i++) {
+    //         console.log("31 and 40");
+    //     }
+    // else if (i = 41)
+    //     for (i; i <= 50; i++) {
+    //         console.log("41 and 50");
+    //     }
+    // else if (i = 50)
+    //     for (i; i <= 50 && i >= 200; i++) {
+    //         console.log("good number");
+    //     }
+    // else {
+    //     console.log("invalid number");
+    // }
+    //12
+    // var i = prompt("enter a number")
+    // if (i == 1)
+    //     for (i; i <= 1; i++) {
+    //         console.log("one");
+    //     }
+    // else if (i == 2)
+    //     for (i; i <= 2; i++) {
+    //         console.log("two");
+    //     }
+    // else if (i == 3)
+    //     for (i; i <= 3; i++) {
+    //         console.log("three");
+    //     }
+    // else if (i == 4)
+    //     for (i; i <= 4; i++) {
+    //         console.log("four");
+    //     }
+    // else if (i == 5)
+    //     for (i; i <= 5; i++) {
+    //         console.log("five");
+    //     }
+    // else(i == 6)
+    // for (i; i <= 6; i++) {
+    //     console.log("invalid");
+    // }
+
+
+//13
+//  var i = prompt("enter a number")
+// if (i = 6)
+//     for (i; i <= 10; i++) {
+//         console.log("proper number");
+//     }
+// else if (i = 7)
+//     for (i; i <= 10; i++) {
+//         console.log("proper number");
+//     }
+// else(i = 11)
+// for (i; i <= 10; i++) {
+//     console.log("valid number");
+// }
+// //14
+// if (i % 2 == 0)
+//     for (var i = 2; i <= 20; i = i + 2) {
+//         console.log(i + "<br>");
+//     }var i = prompt("enter a number");
+
+// else if (i % 3 == 0)
+//     for (var i = 3; i <= 30; i = i + 3) {
+//         console.log(i);
+//     }
+// else(i % 4 == 0)
+// for (var i = 4; i <= 40; i = i + 4) {
+//     console.log("not valid");
+// }
+//15
+// var i = prompt("enter a number")
+
+// if (i == 2)
+//     for (var i = 1; i <= 10; i = i + 2) {
+
+//         console.log(i + "<br/>");
+//     }
